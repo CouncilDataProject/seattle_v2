a500c4afe7bcf7dbd5deaf25e15fb5d0d93da14c
Trung Ngo <trungvngo@gmail.com>
2019-08-15 13:34:25 -0700

    integrate api for home page

--- src/api/eventApi.js
+++ src/api/eventApi.js
@@ -1,10 +1,12 @@
 import * as firebase from "firebase";
-import { getAllResources, getSingleResource } from './baseApi'
-import { fetchJson } from './utils'
+import { getAllResources, getSingleResource, getResourceById } from "./baseApi";
+import { fetchJson } from "./utils";
+import moment from "moment";
+
 const storage = firebase.storage();
 
 export async function getAllEvents() {
-    return getAllResources('event')
+  return getAllResources("event");
 }
 
 /**
@@ -12,71 +14,174 @@ export async function getAllEvents() {
  * @returns {object} returns the same object with the transcript attached
  */
 
-export async function getSingleEvent(eventData){
-    const [firstTranscript] = await getSingleResource('transcript', 'event_id', eventData.id)
-    // some reason the query below not working
-    //const secondQuery = await getSingleResource('file', 'id', transcript.file_id)
-
-    // inefficient yet working
-    const allFiles = await getAllResources('file');
-    const thisFile = allFiles.find(el => el.id === firstTranscript.file_id)
-    
-    // https://firebase.google.com/docs/storage/web/download-files
-    var gsReference = storage.refFromURL(thisFile.uri);
-    const transcript = await gsReference.getDownloadURL()
-        .then(url => fetchJson(url))
-    if (transcript && transcript.data) {
-        return {
-            ...eventData,
-            transcript: transcript.data
-        }
-    }
-    // something went wrong
-    return null
+export async function getEventMinutes(eventId) {
+  try {
+    const eventMinutes = await getSingleResource(
+      "event_minutes_item",
+      "event_id",
+      eventId
+    );
+    return eventMinutes;
+  } catch (e) {
+    return Promise.reject(e);
+  }
 }
 
+export async function getEventMinutesItem(eventMinutesItemId) {
+  try {
+    const {
+      minutes_item_id,
+      created,
+      decision,
+      event_id,
+      index
+    } = await getResourceById("event_minutes_item", eventMinutesItemId);
+    const minutesItem = await getResourceById("minutes_item", minutes_item_id);
 
-export async function getVotesForEvent(eventId){
-    // get all minute items for a single event
-    const extractedMinutes = await getSingleResource('event_minutes_item', 'event_id', eventId)
-    // filter out minute items that are not votes
-    const minuteItemsWithDecisions = extractedMinutes.filter(minute => minute.decision !== null)
-    
-    // instead of getting all minutes items, we could create an array of promises as below
-    const allMinuteItems = await getAllResources('minutes_item')
-    const formattedItems = []
-    minuteItemsWithDecisions.forEach(item => {
-        const thisMinuteItem = allMinuteItems.find(minuteItem => minuteItem.id === item.minutes_item_id)
-        formattedItems.push({
-            matter: thisMinuteItem.matter,
-            name: thisMinuteItem.name,
-            id: item.id,
-            decision: item.decision
-        })
-    })
+    return {
+      id: eventMinutesItemId,
+      created,
+      decision,
+      event_id,
+      index,
+      minutes_item: {
+        id: minutes_item_id,
+        ...minutesItem
+      }
+    };
+  } catch (e) {
+    return Promise.reject(e);
+  }
+}
 
-    const allPeople = await getAllResources('person')   
-    const promises = []
-    formattedItems.forEach(item => {
-        const getVotesForMinuteItem = getSingleResource('vote', 'event_minutes_item_id', item.id)
-        promises.push(getVotesForMinuteItem)
-    })
-    await Promise.all(promises)
-        .then(res => {
-            res.forEach(votesForMinuteItem => {
-                const thisEventMinutesItemId = votesForMinuteItem[0].event_minutes_item_id               
-                const formattedMinutesItem = formattedItems.find(item => item.id === thisEventMinutesItemId)
-                const votesByPerson = []
-                votesForMinuteItem.forEach(unformattedVote => {
-                    const person = allPeople.find(person => person.id === unformattedVote.person_id)
-                    votesByPerson.push({
-                        decision: unformattedVote.decision,
-                        person_id: unformattedVote.person_id,
-                        full_name: person.full_name
-                    })
-                })
-                formattedMinutesItem.formattedIndividualVotes = votesByPerson
-            })
-        })
-    return formattedItems
+export async function getEventBody(bodyId) {
+  try {
+    const body = await getResourceById("body", bodyId);
+    return body;
+  } catch (e) {
+    return Promise.reject(e);
+  }
+}
+
+export async function getEventTranscriptMetadata(eventId) {
+  try {
+    const [firstTranscript] = await getSingleResource(
+      "transcript",
+      "event_id",
+      eventId
+    );
+
+    const transcriptMetadata = await getResourceById(
+      "file",
+      firstTranscript.file_id
+    );
+
+    // https://firebase.google.com/docs/storage/web/download-files
+    const gsReference = storage.refFromURL(transcriptMetadata.uri);
+    const transcript = await gsReference
+      .getDownloadURL()
+      .then(url => fetchJson(url));
+
+    return transcript;
+  } catch (e) {
+    return Promise.reject(e);
+  }
+}
+
+export async function getVotesForEvent(eventId) {
+  // get all minute items for a single event
+  const extractedMinutes = await getSingleResource(
+    "event_minutes_item",
+    "event_id",
+    eventId
+  );
+  // filter out minute items that are not votes
+  const minuteItemsWithDecisions = extractedMinutes.filter(
+    minute => minute.decision !== null
+  );
+
+  // instead of getting all minutes items, we could create an array of promises as below
+  const allMinuteItems = await getAllResources("minutes_item");
+  const formattedItems = [];
+  minuteItemsWithDecisions.forEach(item => {
+    const thisMinuteItem = allMinuteItems.find(
+      minuteItem => minuteItem.id === item.minutes_item_id
+    );
+    formattedItems.push({
+      matter: thisMinuteItem.matter,
+      name: thisMinuteItem.name,
+      id: item.id,
+      decision: item.decision
+    });
+  });
+
+  const allPeople = await getAllResources("person");
+  const promises = [];
+  formattedItems.forEach(item => {
+    const getVotesForMinuteItem = getSingleResource(
+      "vote",
+      "event_minutes_item_id",
+      item.id
+    );
+    promises.push(getVotesForMinuteItem);
+  });
+  await Promise.all(promises).then(res => {
+    res.forEach(votesForMinuteItem => {
+      const thisEventMinutesItemId =
+        votesForMinuteItem[0].event_minutes_item_id;
+      const formattedMinutesItem = formattedItems.find(
+        item => item.id === thisEventMinutesItemId
+      );
+      const votesByPerson = [];
+      votesForMinuteItem.forEach(unformattedVote => {
+        const person = allPeople.find(
+          person => person.id === unformattedVote.person_id
+        );
+        votesByPerson.push({
+          decision: unformattedVote.decision,
+          person_id: unformattedVote.person_id,
+          full_name: person.full_name
+        });
+      });
+      formattedMinutesItem.formattedIndividualVotes = votesByPerson;
+    });
+  });
+  return formattedItems;
+}
+
+export async function getEventById(id) {
+  try {
+    const event = await getResourceById("event", id);
+    const body = await getEventBody(event.body_id);
+    const minutes = await getEventMinutes(id);
+    const minutesItems = await Promise.all(
+      minutes.map(({ id }) => getEventMinutesItem(id))
+    );
+    const transcript = await getEventTranscriptMetadata(id);
+
+    return {
+      id,
+      body,
+      minutes: minutesItems,
+      transcript: transcript.data
+    };
+  } catch (e) {
+    return Promise.reject(e);
+  }
+}
+
+export async function getBasicEventById(id) {
+  try {
+    const event = await getResourceById("event", id);
+    const body = await getEventBody(event.body_id);
+
+    return {
+      id,
+      name: body.name,
+      description: body.description,
+      date: body.created.toDate().toString()
+    };
+  } catch (e) {
+    return Promise.reject(e);
+  }
 }
--- src/containers/EventCardGroup.jsx
+++ src/containers/EventCardGroup.jsx
@@ -1,10 +1,26 @@
 import React from "react";
-import mockEvents from "../data/events";
+import { getAllEvents, getEventById, getBasicEventById } from "../api/eventApi";
 import EventCardGroup from "../components/EventCardGroup";
+const pagelimit = 9;
 
 const EventCardGroupContainer = () => {
-  //   TODO: fetch events
-  return <EventCardGroup events={mockEvents} />;
+  const [visibleEvents, setVisibleEvents] = React.useState([]);
+
+  React.useEffect(() => {
+    // Fetch events once
+    try {
+      (async () => {
+        const allEvents = await getAllEvents();
+        const basicEventData = await Promise.all(
+          allEvents.slice(0, pagelimit).map(({ id }) => getBasicEventById(id))
+        );
+        setVisibleEvents(basicEventData);
+      })();
+    } catch (e) {
+      // log and display error message
+    }
+  }, []);
+  return <EventCardGroup events={visibleEvents} />;
 };
 
 export default EventCardGroupContainer;
