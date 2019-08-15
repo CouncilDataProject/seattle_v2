7160f930e5acdd2621cd953bd0b799e472958709
Nathan Peterson <natperson@gmail.com>
2019-08-15 16:20:45 -0700

    adds voting records to event

--- src/components/EventVotingPane.jsx
+++ src/components/EventVotingPane.jsx
@@ -1,46 +1,95 @@
 import React from 'react'
-import { Icon, Label, Menu, Table, TableBody } from 'semantic-ui-react'
+import { Table, Loader } from 'semantic-ui-react'
+import styled from "@emotion/styled";
 
 const Headers = [
     {
         accessor: 'matter',
         text: 'Matter',
+        width: '2'
+    },
+    {
+        accessor: 'decision',
+        text: 'Decision',
+        width: '2',
     },
     {
         accessor: 'name',
         text: 'Name',
+        width: '6'
     },
     {
-        accessor: 'decision',
-        text: 'Decision'
+        accessor: 'votingRecords',
+        text: 'Votes',
+        width: '4'
     }
 ]
 
+const LoadingContainer = styled('div')({
+    minHeight: '100px',
+    width: '100%',
+})
+
+const MiniTable = styled('div')({
+    display: 'flex'
+})
+
+const MiniTableCell = styled('div')({
+    width: '50%'
+})
+
+const TableData = ({votingData}) => (
+<Table striped>
+    <Table.Header>
+        <Table.Row>
+            {Headers.map(headerMetaData => (<Table.HeaderCell 
+                width={headerMetaData.width}>{headerMetaData.text}
+                </Table.HeaderCell>
+            ))}
+        </Table.Row>
+    </Table.Header>
+    <Table.Body>
+        {votingData.map(votingDatum => (
+            <Table.Row>
+                {Headers.map(headerMetaData => {
+                    if(headerMetaData.accessor !== 'votingRecords') {
+                        return <Table.Cell>{votingDatum[headerMetaData.accessor]}</Table.Cell>
+                    }
+                    return (
+                        <Table.Cell>
+                            {votingDatum.formattedIndividualVotes.map(record => {
+                                return (
+                                <MiniTable>
+                                    <MiniTableCell>{record.full_name}</MiniTableCell>
+                                    <MiniTableCell>{record.decision}</MiniTableCell>
+                                </MiniTable>
+                                )
+                            })}
+                        </Table.Cell>
+                    )
+                })}
+            </Table.Row>
+        ))}
+    </Table.Body>
+</Table>
+)
+
 const EventVotingPane = ({ votingData }) => {
-    console.log('voting data', votingData)
     if(!votingData) {
-        return <div>spinner</div>
+        return (
+            <LoadingContainer>
+                <Loader active />
+            </LoadingContainer>
+        ) 
+    }
+    if(!votingData.length) {
+        return (
+            <div>No votes found for this event.</div>
+        )
     }
     return (
     <div style={{width: '100%'}}>
-        <Table celled>
-            <Table.Header>
-                <Table.Row>
-                    {Headers.map(headerMetaData => {
-                        return <Table.HeaderCell>{headerMetaData.text}</Table.HeaderCell>
-                    })}
-                </Table.Row>
-            </Table.Header>
-            <Table.Body>
-                {votingData.map(votingDatum => (
-                    <Table.Row>
-                        {Headers.map(headerMetaData => {
-                            return <Table.HeaderCell>{votingDatum[headerMetaData.accessor]}</Table.HeaderCell>
-                        })}
-                    </Table.Row>
-                ))}
-            </Table.Body>
-        </Table>
+       <TableData votingData={votingData}/>
     </div>
     )
 }
