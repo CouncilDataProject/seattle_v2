da33cd940b87b14115b74fbb1d3367ef75d84a40
Trung Ngo <trungvngo@gmail.com>
2019-08-15 16:36:17 -0700

    wip: search bar

--- src/containers/EventCardGroup.jsx
+++ src/containers/EventCardGroup.jsx
@@ -1,10 +1,21 @@
 import React from "react";
-import { getAllEvents, getEventById, getBasicEventById } from "../api/eventApi";
+import { Input } from "semantic-ui-react";
+import { getAllEvents, getBasicEventById } from "../api/eventApi";
 import EventCardGroup from "../components/EventCardGroup";
-const pagelimit = 9;
+import isSubstring from "../utils/isSubstring";
+
+const pagelimit = 10;
 
 const EventCardGroupContainer = () => {
+  const [searchQuery, setSearchQuery] = React.useState();
+  const [events, setEvents] = React.useState([]);
   const [visibleEvents, setVisibleEvents] = React.useState([]);
+
+  const handleSearch = (e, { value }) => {
+    setSearchQuery(value);
+    // filter all events by name and set visible event
+    setVisibleEvents(events.filter(({ name }) => isSubstring(name, value)));
+  };
 
   React.useEffect(() => {
     // Fetch events once
@@ -12,15 +23,25 @@ const EventCardGroupContainer = () => {
       (async () => {
         const allEvents = await getAllEvents();
         const basicEventData = await Promise.all(
-          allEvents.slice(0, pagelimit).map(({ id }) => getBasicEventById(id))
+          allEvents.map(({ id }) => getBasicEventById(id))
         );
-        setVisibleEvents(basicEventData);
+        setEvents(basicEventData);
+        setVisibleEvents(basicEventData.slice(0, pagelimit));
       })();
     } catch (e) {
       // log and display error message
     }
   }, []);
-  return <EventCardGroup events={visibleEvents} />;
+  return (
+    <React.Fragment>
+      <Input
+        placeholder="Search by event name"
+        value={searchQuery}
+        onChange={handleSearch}
+      />
+      <EventCardGroup events={visibleEvents} />
+    </React.Fragment>
+  );
 };
 
 export default EventCardGroupContainer;
