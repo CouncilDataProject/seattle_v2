52af0802e1cd3c98e7deef9627bef2b3aa292bba
Trung Ngo <trungvngo@gmail.com>
2019-08-16 13:16:18 -0700

    add page for all events

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,15 +5,15 @@ import styled from "@emotion/styled";
 
 const Brand = styled.h1({});
 
-const HeadingTab = styled('span')({
-  transition: '.8s all',
-  borderRadius: '5px',
-  padding: '1rem .5rem',
-  ':hover': {
-    backgroundColor: '#F5F5F5',
-    color: 'black'
+const HeadingTab = styled("span")({
+  transition: ".8s all",
+  borderRadius: "5px",
+  padding: "1rem .5rem",
+  ":hover": {
+    backgroundColor: "#F5F5F5",
+    color: "black"
   }
-})
+});
 
 const Header = () => (
   <React.Fragment>
@@ -26,11 +26,11 @@ const Header = () => (
     </Segment>
     <Segment vertical>
       <Container>
-        <Link to="/">
+        <Link to="/events">
           <HeadingTab>Events</HeadingTab>
         </Link>
         <Link to="/people">
-          <HeadingTab style={{marginLeft: '10px'}}>People</HeadingTab>
+          <HeadingTab style={{ marginLeft: "10px" }}>People</HeadingTab>
         </Link>
       </Container>
     </Segment>
---
+++ src/containers/AllEvents.jsx
@@ -0,0 +1,89 @@
+import React from "react";
+import { Select } from "semantic-ui-react";
+import moment from "moment";
+import styled from "@emotion/styled";
+import { getAllEvents, getBasicEventById } from "../api/eventApi";
+import EventCardGroup from "../components/EventCardGroup";
+
+const FiltersSection = styled.div({
+  margin: "1em 0 3em !important"
+});
+
+const ResultCount = styled.span({
+  display: "block",
+  color: "grey",
+  paddingLeft: "15px",
+  paddingTop: "8px"
+});
+
+const LoadingText = styled.span({
+  color: "grey",
+  fontWeight: "700",
+  fontSize: "18px"
+});
+
+const Results = styled.div({
+  paddingLeft: "15px"
+});
+
+const DateFilter = styled(Select)({});
+
+const EventCardGroupContainer = ({ query }) => {
+  const [allEvents, setAllEvents] = React.useState([]);
+  const [isLoading, setIsLoading] = React.useState(false);
+  const [visibleEvents, setVisibleEvents] = React.useState([]);
+
+  const handleDateFilter = (e, { value }) => {
+    if (value === "all") {
+      setVisibleEvents(allEvents);
+    } else {
+      const comparisonDate = moment().subtract(value, "months");
+      const isAfter = date => moment(date).isAfter(comparisonDate);
+      setVisibleEvents(allEvents.filter(({ date }) => isAfter(date)));
+    }
+  };
+
+  React.useEffect(() => {
+    (async () => {
+      setIsLoading(true);
+      const allEvents = await getAllEvents();
+      const basicEventData = await Promise.all(
+        allEvents.map(({ id }) => getBasicEventById(id))
+      );
+      setAllEvents(basicEventData);
+      setVisibleEvents(basicEventData);
+      setIsLoading(false);
+    })();
+  }, []);
+
+  const filterOptions = [
+    { key: "-1", value: "all", text: "All" },
+    { key: "0.25", value: "0.25", text: "Past week" },
+    { key: "1", value: "1", text: "Past month" },
+    { key: "3", value: "3", text: "Past 3 months" },
+    { key: "6", value: "6", text: "Past 6 months" },
+    { key: "12", value: "12", text: "Past year" }
+  ];
+
+  return (
+    <React.Fragment>
+      <FiltersSection>
+        <DateFilter
+          placeholder="Filter by date range"
+          options={filterOptions}
+          onChange={handleDateFilter}
+        />
+        <ResultCount>{visibleEvents.length} results</ResultCount>
+      </FiltersSection>
+      <Results>
+        {isLoading ? (
+          <LoadingText>Loading...</LoadingText>
+        ) : (
+          <EventCardGroup events={visibleEvents} />
+        )}
+      </Results>
+    </React.Fragment>
+  );
+};
+
+export default EventCardGroupContainer;
