879848c0766d3818fac27601a44ddddcc0206691
JacksonMaxfield <jmaxfieldbrown@gmail.com>
2019-09-03 23:12:38 -0700

    Add isString utility function because JavaScript

--- src/api/utils.js
+++ src/api/utils.js
@@ -2,3 +2,7 @@ export async function fetchJson(url){
     return fetch(url)
         .then(res => res.json())
 };
+
+export function isString(s) {
+    return typeof(s) === "string" || s instanceof String;
+};
