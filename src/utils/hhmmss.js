c679f8a613b77414279f0f745ab4aa514fea5d9c
Jackson Brown <jmaxfieldbrown@gmail.com>
2019-09-09 15:05:44 -0700

    Fix lint error

--- src/utils/hhmmss.js
+++ src/utils/hhmmss.js
@@ -1,5 +1,5 @@
 function hhmmss(sec_num) {
-  var sec_num = parseInt(sec_num, 10); // don't forget the second param
+  sec_num = parseInt(sec_num, 10); // don't forget the second param
   var hours = Math.floor(sec_num / 3600);
   var minutes = Math.floor((sec_num - hours * 3600) / 60);
   var seconds = sec_num - hours * 3600 - minutes * 60;
