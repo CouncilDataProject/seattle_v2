3f7fb6b5d843774d1684f0bcb6f666b9baf621df
To Huynh <huynh.nto@gmail.com>
2019-09-17 17:34:17 -0700

    sort votes and add links for each person

--- src/components/VotingTable.jsx
+++ src/components/VotingTable.jsx
@@ -4,95 +4,109 @@ import { Link } from 'react-router-dom';
 import styled from "@emotion/styled";
 
 const Headers = [
-    {
-        accessor: 'matter',
-        text: 'Matter',
-        width: '2'
-    },
-    {
-        accessor: 'decision',
-        text: 'Decision',
-        width: '2',
-    },
-    {
-        accessor: 'name',
-        text: 'Name',
-        width: '6'
-    },
-    {
-        accessor: 'votingRecords',
-        text: 'Vote',
-        width: '4'
-    }
+  {
+    accessor: 'matter',
+    text: 'Matter',
+    width: '2'
+  },
+  {
+    accessor: 'decision',
+    text: 'Decision',
+    width: '1'
+  },
+  {
+    accessor: 'name',
+    text: 'Name',
+    width: '6'
+  },
+  {
+    accessor: 'votingRecords',
+    text: 'Vote',
+    width: '5'
+  }
 ]
 
 const MiniTable = styled('div')({
-    display: 'flex'
-})
+  display: 'flex'
+});
 
-const MiniTableCell = styled('div')({
-    width: '50%'
-})
+const Person = styled('div')({
+  width: '70%'
+});
+
+const Decision = styled('div')({
+  width: '30%'
+});
+
+const StyledTableHeader = styled(Table.Header)({
+  '@media(max-width:767px)': {
+    display: 'none !important'
+  }
+});
 
 
 const VotingTable = ({ votingData, isPerson }) => (
-    <Table striped>
-        <Table.Header>
-            <Table.Row>
-                {Headers.map(headerMetaData => (<Table.HeaderCell
-                    key={headerMetaData.text}
-                    width={headerMetaData.width}>{headerMetaData.text}
-                    </Table.HeaderCell>
-                ))}
-                {/* If we are rendering votes for a person, we want to link to the event */}
-                {isPerson && <Table.HeaderCell>Event</Table.HeaderCell>}
-            </Table.Row>
-        </Table.Header>
-        <Table.Body>
-            {votingData.map(votingDatum => (
-                <Table.Row>
-                    {Headers.map(headerMetaData => {
-                        if(headerMetaData.accessor !== 'votingRecords') {
-                            return (
-                                <Table.Cell key={headerMetaData.accessor}>
-                                    {votingDatum[headerMetaData.accessor]}
-                                </Table.Cell>
-                            )
-                        }
-                        if (votingDatum.formattedIndividualVotes) {
-                            return (
-                                <Table.Cell>
-                                    {votingDatum.formattedIndividualVotes.map(record => {
-                                        return (
-                                        <MiniTable key={record.full_name}>
-                                            <MiniTableCell>{record.full_name}</MiniTableCell>
-                                            <MiniTableCell>
-                                                {record.decision}
-                                            </MiniTableCell>
-                                        </MiniTable>
-                                        )
-                                    })}
-                                </Table.Cell>
-                            )
-                        } else {
-                            return (
-                            <React.Fragment>
-                                <Table.Cell>
-                                    {votingDatum.voteForPerson}
-                                </Table.Cell>
-                                <Table.Cell>
-                                    <Link to={`/events/${votingDatum.eventId}`}>
-                                        <Icon name="linkify"/>
-                                    </Link>
-                                </Table.Cell>
-                            </React.Fragment>
-                            )
-                        }
-                    })}
-                </Table.Row>
-            ))}
-        </Table.Body>
-    </Table>
+  <Table striped>
+    <StyledTableHeader>
+      <Table.Row>
+        {Headers.map(headerMetaData => (<Table.HeaderCell
+          key={headerMetaData.text}
+          width={headerMetaData.width}>{headerMetaData.text}
+        </Table.HeaderCell>
+        ))}
+        {/* If we are rendering votes for a person, we want to link to the event */}
+        {isPerson && <Table.HeaderCell key={'Event'}>Event</Table.HeaderCell>}
+      </Table.Row>
+    </StyledTableHeader>
+    <Table.Body>
+      {votingData.map(votingDatum => (
+        <Table.Row key={votingDatum.id}>
+          {Headers.map(headerMetaData => {
+            if (headerMetaData.accessor !== 'votingRecords') {
+              return (
+                <Table.Cell key={headerMetaData.accessor}>
+                  {votingDatum[headerMetaData.accessor]}
+                </Table.Cell>
+              )
+            }
+            if (votingDatum.formattedIndividualVotes) {
+              return (
+                <Table.Cell key={headerMetaData.accessor}>
+                  {votingDatum.formattedIndividualVotes.map(record => {
+                    return (
+                      <MiniTable key={record.full_name}>
+                        <Person>
+                          <Link to={`/people/${record.person_id}`}>
+                            {record.full_name}
+                          </Link>
+                        </Person>
+                        <Decision>
+                          {record.decision}
+                        </Decision>
+                      </MiniTable>
+                    )
+                  })}
+                </Table.Cell>
+              )
+            } else {
+              return (
+                <React.Fragment key={headerMetaData.accessor}>
+                  <Table.Cell>
+                    {votingDatum.voteForPerson}
+                  </Table.Cell>
+                  <Table.Cell>
+                    <Link to={`/events/${votingDatum.eventId}`}>
+                      <Icon name="linkify" />
+                    </Link>
+                  </Table.Cell>
+                </React.Fragment>
+              )
+            }
+          })}
+        </Table.Row>
+      ))}
+    </Table.Body>
+  </Table>
 )
 
 export default VotingTable
