4f87ac9ca499244ca578d0be6e0082b1a8ead892
Tim Ganter <1911000+timganter@users.noreply.github.com>
2019-11-23 22:02:10 -0800

    Filter votes by vote cast and styling tweaks.

--- src/components/Person.jsx
+++ src/components/Person.jsx
@@ -1,32 +1,54 @@
-import React from 'react';
-import VotingTable from './VotingTable'
-import { Card } from 'semantic-ui-react'
-import styled from "@emotion/styled";
+import React, { useState }                       from 'react';
+import { Checkbox, Form, Header, Grid, Segment } from 'semantic-ui-react';
+import _                                         from 'lodash';
+import VotingTable                               from './VotingTable';
 
-const FlexContainer = styled('div')({
-    display: 'flex',
-    justifyContent: 'center',
-    flexDirection: 'column',
-    alignItems: 'center'
-})
+const cssCheckbox = { marginRight: '0.5em' }
 
 const Person = ({personHistory: {full_name, email, phone, website, votes}}) => {
-    return (
-    <FlexContainer>
-        <Card style={{padding: '1rem', width: '100%'}}>
-            <Card.Header>{full_name}</Card.Header>
-            <a href={`mailto:${email}`}>{email}</a>
-            <div>{phone}</div>
-            <div>
-                Website:  
-                <a  target={'_blank'}
-                    rel="noopener noreferrer"
-                    href={website}>{website}</a>
-            </div>
-        </Card>
-        <VotingTable votingData={votes} isPerson={true} />
-    </FlexContainer>
-    )
+  // == Get unique vote cast types from votes list.
+  const votingOptions = _.uniq(votes.map(vote => vote.voteForPerson))
+  // == Selected votes for checkboxes.
+  const [selectedVoteFilters, setSelectedVoteFilters] = useState([])
+  // == Votes filtered to be displayed in table.
+  const [filteredVotes, setFilteredVotes] = useState(votes)
+  
+  // == Handler for checkboxes and refiltering table results.
+  const onSelectedVoteFilterChange = (event, { label }) => {
+    if (selectedVoteFilters.includes(label)) {
+      setSelectedVoteFilters(_.without(selectedVoteFilters, label))
+    } else {
+      selectedVoteFilters.push(label)
+      setSelectedVoteFilters(selectedVoteFilters)
+    }
+
+    setFilteredVotes(_.filter(votes, vote => selectedVoteFilters.includes(vote.voteForPerson)))
+  }
+
+  // == Make a checkbox for each vote cast type in the voting table.
+  const filterVotingOptions = votingOptions.map((votingOption, index) => <Checkbox style={cssCheckbox} key={index} label={votingOption} onChange={onSelectedVoteFilterChange} />)
+
+  return <React.Fragment>
+    <Header as="h1">{full_name}</Header>
+    <Grid columns={1} stackable>
+      <Grid.Column>
+        <Segment>
+          <Header as="h2">Contact information</Header>
+          <strong>Email:</strong> <a href={`mailto:${email}`}>{email}</a>
+          <div><strong>Phone:</strong> {phone}</div>
+          <div><strong>Website:</strong> <a target={'_blank'} rel="noopener noreferrer" href={website}>{website}</a></div>
+        </Segment>
+      </Grid.Column>
+      <Grid.Column>
+        <Segment>
+          <Header as="h2">Voting record</Header>
+          <Header as="h3">Filter by vote cast</Header>
+          <Form>{ filterVotingOptions }</Form>
+          <VotingTable votingData={filteredVotes} isPerson={true} />
+         </Segment>
+      </Grid.Column>
+    </Grid>
+  </React.Fragment>
 }
 
 export default Person;
