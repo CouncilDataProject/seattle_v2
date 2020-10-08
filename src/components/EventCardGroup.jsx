cae59472430684d73c6dafb22eb2370c836c7f7f
To Huynh <huynh.nto@gmail.com>
2020-10-07 19:09:49 -0700

    Change to 1 columns of meetings

--- src/components/EventCardGroup.jsx
+++ src/components/EventCardGroup.jsx
@@ -3,7 +3,7 @@ import { Card, Pagination, Grid } from "semantic-ui-react";
 
 import EventCard from "./EventCard";
 
-const NUMBER_OF_EVENTS_PER_PAGE = 20;
+const NUMBER_OF_EVENTS_PER_PAGE = 10;
 
 const EventCardGroup = ({ events, query }) => {
   // the current page in the pagination navigation
@@ -18,38 +18,34 @@ const EventCardGroup = ({ events, query }) => {
   )
 
   return (
-    <Grid stackable columns="equal">
-      <Grid.Row>
-        <Grid.Column>{`${events.length} ${events.length > 1 ? "results" : "result"}`}</Grid.Column>
-        <Grid.Column>
-          <Pagination
-            activePage={activePage}
-            boundaryRange={0}
-            siblingRange={1}
-            ellipsisItem={null}
-            size="small"
-            onPageChange={onPageChange}
-            totalPages={Math.ceil(events.length / NUMBER_OF_EVENTS_PER_PAGE)}
-          />
-        </Grid.Column>
-      </Grid.Row>
-      <Grid.Row>
-        <Grid.Column>
-          <Card.Group stackable itemsPerRow={2}>
-            {activeEvents.map(({ name, date, description, id }) => (
-              <EventCard
-                key={id}
-                name={name}
-                date={date}
-                description={description}
-                link={`/events/${id}`}
-                query={query}
-              />
-            ))}
-          </Card.Group>
-        </Grid.Column>
-      </Grid.Row>
-
+    <Grid>
+      <Grid.Column width={16}>{`${events.length} ${events.length > 1 ? "results" : "result"}`}</Grid.Column>
+      <Grid.Column width={16}>
+        <Card.Group>
+          {activeEvents.map(({ name, date, description, id }) => (
+            <EventCard
+              key={id}
+              name={name}
+              date={date}
+              description={description}
+              link={`/events/${id}`}
+              query={query}
+            />
+          ))}
+        </Card.Group>
+      </Grid.Column>
+      <Grid.Column width={16}>
+        <Pagination
+          pointing
+          secondary
+          activePage={activePage}
+          boundaryRange={0}
+          siblingRange={1}
+          ellipsisItem={null}
+          onPageChange={onPageChange}
+          totalPages={Math.ceil(events.length / NUMBER_OF_EVENTS_PER_PAGE)}
+        />
+      </Grid.Column>
     </Grid>
   );
 };
